90affe2f19eef32cd05182df9e44c0a3a62a5f0a
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-01 23:05:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
-
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -18,13 +18,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById("65bbd083d67d562ebdbaec33")
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -33,7 +32,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(client => {
-  console.log(client);
   app.listen(3000);
 });
+
 
