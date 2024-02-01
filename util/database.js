dc3519fe8029d6856db05893b69631b2bb61d17d
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-01 22:26:22 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,17 +1,28 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
+    'mongodb+srv://ranvi:2580@Ranvi@cluster0.j1shrhc.mongodb.net/?retryWrites=true&w=majority'
   )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  throw "No database found!";
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
