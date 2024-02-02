3d9de3af9d9916b08afb0dfed0c8a76ac2a81436
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-02 11:03:26 +0530

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,10 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl, null, req.user._id);
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
     .save()
-    .then(() => {
+    .then(result => {
+      // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
