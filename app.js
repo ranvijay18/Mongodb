af677f996d60f830644ac26663cf951849f87853
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-02 10:27:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("65bbd083d67d562ebdbaec33")
     .then(user => {
-      req.user = user;
+     req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
