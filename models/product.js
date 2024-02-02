699d4f9845836086b89912258ea96716203d6c1b
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-02 11:06:13 +0530

    Update product.js

--- models/product.js
+++ models/product.js
@@ -2,12 +2,13 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
 
   save() {
@@ -18,18 +19,16 @@ class Product {
       dbOp = db
         .collection('products')
         .updateOne({ _id: this._id }, { $set: this });
-        console.log("hii");
     } else {
       dbOp = db.collection('products').insertOne(this);
-      console.log("lol");
     }
     return dbOp
-      // .then(result => {
-      //   console.log(result);
-      // })
-      // .catch(err => {
-      //   console.log(err);
-      // });
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   static fetchAll() {
@@ -39,7 +38,7 @@ class Product {
       .find()
       .toArray()
       .then(products => {
-        // console.log(products);
+        console.log(products);
         return products;
       })
       .catch(err => {
@@ -54,7 +53,7 @@ class Product {
       .find({ _id: new mongodb.ObjectId(prodId) })
       .next()
       .then(product => {
-        // console.log(product);
+        console.log(product);
         return product;
       })
       .catch(err => {
