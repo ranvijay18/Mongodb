a44a7c0eb96fd6b05317b632d015d4839049c8d4
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2024-02-02 10:25:16 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -14,6 +14,15 @@ class User {
     return db.collection('users').insertOne(this);
   }
 
+  addToCart(product){
+  // const cartProduct = this.cart.items.findIndex(cp => {
+  //   return cp._id === product._id;
+  // });
+  const updatedCart = {items: [{ productId: new ObjectId(product._id), quantity:1}]};
+  const db = getDb();
+  return db.collection('users').updateOne({_id: new ObjectId(this._id)}, {$set: {cart: updatedCart}})
+  }
+
   static findById(userId){
   const db = getDb();
   return db.collection('users').findOne({_id: new ObjectId(userId)})
